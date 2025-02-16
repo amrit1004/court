fab3adfecdba4e1c2bd8c35c4300a6328c6d244f
Amrit <amritsinghal2211@gmail.com>
2025-02-16 12:18:09 +0530

    added changes to connection string

--- helpers/db-utils.js
+++ helpers/db-utils.js
@@ -1,14 +1,5 @@
 import { MongoClient, ObjectID } from 'mongodb';
-
-// const username = process.env.MONGODB_USERNAME;
-// const password = process.env.MONGODB_PASSWORD;
-// const cluster = process.env.MONGODB_CLUSTER;
-// const database = process.env.MONGODB_DATABASE;
-
-// const MONGODB_URI = `mongodb+srv://${username}:${password}@${cluster}.3svps.mongodb.net/${database}?retryWrites=true&w=majority`;
-
-const MONGODB_URI =
-  'mongodb+srv://amritsinghal2211:nyvd63nidJhNahge@cluster0.dpeyq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(MONGODB_URI, {
